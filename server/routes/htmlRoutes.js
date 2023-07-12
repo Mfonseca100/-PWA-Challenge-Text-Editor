1c85ed7de6e6ef588bb66098a1b9140bc1f38900
Edith <rissafonseca@yahoo.com>
2023-07-11 23:49:50 -0500

    fixing route issue

--- server/routes/htmlRoutes.js
+++ server/routes/htmlRoutes.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
+
 module.exports = (app) =>
   app.get('/', (req, res) =>
-    res.sendFile(path.resolve(__dirname, '..', 'PWA-Challenge-Text-Editor', 'client', 'dist', 'index.html'))
+    res.sendFile(path.resolve(__dirname, '..', 'client', 'dist', 'index.html'))
   );
